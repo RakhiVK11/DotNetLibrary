d75b373c1bfeb7c2675c5a780896fe3ef0ea4db7
Rakhi Vijay Kumar <rakhiv.kumar@ltts.com>
2021-05-03 09:21:52 +0530

    Add project files.

---
+++ DotNetLibrary/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Humanizer;
+
+namespace DotNetLibrary
+{
+    public class Class1
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("car".Pluralize());
+            Console.WriteLine("octopus".Pluralize());
+        }
+    }
+}
